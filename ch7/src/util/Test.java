25f89cfbd711a38e38874058927dd0514097ae64
sunghoon <tjdgns_573@naver.com>
2023-03-17 10:43:21 +0900

    인스턴스 메소드 작성

--- ch7/src/util/Test.java
+++ ch7/src/util/Test.java
@@ -1,9 +1,17 @@
 package util;
 
 public class Test {
+<<<<<<< master
 	
 	private String name;
 	private int no;
 	private String tel;
+	
+	public void print() {}
+	public int sum() {
+		return 0;
+	}
+=======
+>>>>>>> 69889de 인스턴스 변수 생성
 
 }
